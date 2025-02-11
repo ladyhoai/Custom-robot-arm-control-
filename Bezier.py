9c793174042afccdc9338110654d9cbe7032287c
Joseph <nguyenxuankien3132004@gmail.com>
2025-02-12 02:30:59 +1100

    Generate brushstroke for robot arm

---
+++ Bezier.py
@@ -0,0 +1,238 @@
+import numpy as np
+import matplotlib.pyplot as pl
+import scipy as sp
+import sys
+
+# eps = sys.float_info.epsilon
+def bernstein(n, i, t):
+    return sp.special.comb(n, i) * (t ** i) * ((1 - t) ** (n - i))
+
+def compute_sibson_weights(query_point,  vor, points):
+    newPointList = np.vstack([points, query_point])
+    newVor = Voronoi(newPointList)
+
+    newPointIndex = len(points)
+
+    # Index of the new point in the new Voronoi diagram
+    naturalNeighbours = newVor.ridge_points[newVor.ridge_points[:, 0] == newPointIndex, 1]
+
+    # Exclude the new point
+    naturalNeighbours = naturalNeighbours[naturalNeighbours < len(points)]
+
+    stolenAreas = {}
+    newCell = Polygon(newVor.vertices[newVor.regions[newVor.point_region[newPointIndex]]])
+
+    for neighbour in naturalNeighbours:
+        # Get the original Voronoi cell of the neighbor
+        originalCell = Polygon(vor.vertices[vor.regions[vor.point_region[neighbour]]])
+
+        # Compute the intersection
+        stolenArea = newCell.intersection(originalCell).area
+        stolenAreas[neighbour] = stolenArea
+
+    totalStolenArea = sum(stolenAreas.values())
+    sibsonWeights = {neighbour : area / totalStolenArea for neighbour, area in stolenAreas.items()}
+
+    return sibsonWeights
+
+def bezier_curve(t, P):
+    """Compute a cubic Bézier curve given control points P."""
+    return (1 - t)**3 * P[0] + 3 * (1 - t)**2 * t * P[1] + 3 * (1 - t) * t**2 * P[2] + t**3 * P[3]
+
+def in_box(towers, bounding_box):
+    return np.logical_and(np.logical_and(bounding_box[0] <= towers[:, 0],
+                                         towers[:, 0] <= bounding_box[1]),
+                          np.logical_and(bounding_box[2] <= towers[:, 1],
+                                         towers[:, 1] <= bounding_box[3]))
+
+
+def voronoi(towers, bounding_box):
+    # Select towers inside the bounding box
+    i = in_box(towers, bounding_box)
+    # Mirror points
+    points_center = towers[i, :]
+    points_left = np.copy(points_center)
+    points_left[:, 0] = bounding_box[0] - (points_left[:, 0] - bounding_box[0])
+    points_right = np.copy(points_center)
+    points_right[:, 0] = bounding_box[1] + (bounding_box[1] - points_right[:, 0])
+    points_down = np.copy(points_center)
+    points_down[:, 1] = bounding_box[2] - (points_down[:, 1] - bounding_box[2])
+    points_up = np.copy(points_center)
+    points_up[:, 1] = bounding_box[3] + (bounding_box[3] - points_up[:, 1])
+    points = np.append(points_center, np.append(np.append(points_left, points_right, axis=0), np.append(points_down, points_up, axis=0), axis=0), axis=0)
+    # Compute Voronoi
+    vor = sp.spatial.Voronoi(points)
+    # Filter regions
+    regions = []
+    for region in vor.regions:
+        flag = True
+        for index in region:
+            if index == -1:
+                flag = False
+                break
+            else:
+                x = vor.vertices[index, 0]
+                y = vor.vertices[index, 1]
+                eps = sys.float_info.epsilon
+
+                if not(bounding_box[0] - eps <= x and x <= bounding_box[1] + eps and
+                       bounding_box[2] - eps <= y and y <= bounding_box[3] + eps):
+                    flag = False
+                    break
+        if region != [] and flag:
+            regions.append(region)
+    vor.filtered_points = points_center
+    vor.filtered_regions = regions
+    return vor
+
+def centroid_region(vertices):
+    # Polygon's signed area
+    A = 0
+    # Centroid's x
+    C_x = 0
+    # Centroid's y
+    C_y = 0
+    for i in range(0, len(vertices) - 1):
+        s = (vertices[i, 0] * vertices[i + 1, 1] - vertices[i + 1, 0] * vertices[i, 1])
+        A = A + s
+        C_x = C_x + (vertices[i, 0] + vertices[i + 1, 0]) * s
+        C_y = C_y + (vertices[i, 1] + vertices[i + 1, 1]) * s
+    A = 0.5 * A
+    C_x = (1.0 / (6.0 * A)) * C_x
+    C_y = (1.0 / (6.0 * A)) * C_y
+    return np.array([[C_x, C_y]])
+
+
+# Define grid size
+N = 100
+x = np.linspace(-1, 1, N)
+y = np.linspace(-1, 1, N)
+X, Y = np.meshgrid(x, y)
+
+#IMPORTANT, this number changes everything
+gradeBezier = 4
+
+# Control points for an S-shaped cubic Bézier curve
+P = np.array([[-0.8, -0.8], [-0.4, -0.8], [0.2, 1], [0.8, 0.8]])
+
+# Generate Bézier curve points
+T = np.linspace(0, 1, 18)
+print(len(T))
+bezier_points = np.array([bezier_curve(t, P) for t in T])
+
+# Create Delaunay triangulation using scipy's Delaunay
+delaunay = sp.spatial.Delaunay(bezier_points)
+
+# Compute convex hull
+hull = sp.spatial.ConvexHull(bezier_points)
+
+grid_points = np.vstack([X.ravel(), Y.ravel()]).T
+
+query_point = np.array([-0.25, -0.25])
+
+bounding_box = np.array([-1, 1, -1, 1]) # [x_min, x_max, y_min, y_max]
+
+vor = voronoi(bezier_points, bounding_box)
+
+fig = pl.figure()
+
+ax = fig.gca()
+
+np.set_printoptions(precision=3, suppress=True)  # Set printing precision
+ax.set_xlim([-1.1, 1.1])
+ax.set_ylim([-1.1, 1.1])
+
+area = []
+
+# Calculating the area for each polygon in the Voronoi diagram
+for i in range(len(vor.filtered_regions)):
+    # ax.plot(bezier_points[i, 0], bezier_points[i, 1], 'b.')
+    # pl.draw()
+    # pl.pause(0.5)
+
+    regionIndex = vor.point_region[i]
+    region = vor.regions[regionIndex]
+    vertices = vor.vertices[region + [region[0]], :]
+
+    area = area +  [sp.spatial.ConvexHull(vor.vertices[region, :]).volume]
+
+    # ax.plot(vertices[:, 0], vertices[:, 1], 'k-')
+
+    # pl.draw()
+    # pl.pause(0.5)
+slopeListX = []
+slopeListY = []
+failedVor = 0
+
+for point in grid_points:
+    if delaunay.find_simplex(point) != -1:
+
+        bezier_points = np.vstack([bezier_points, point])
+        vorUpdated = voronoi(bezier_points, bounding_box)
+
+        areaUpdated = []
+        areaPercentage = []
+        # ax.cla()
+        # ax.scatter(bezier_points[:, 0], bezier_points[:, 1], color='red', zorder=5)
+        # pl.draw()
+
+        for i in range(len(vorUpdated.filtered_regions)):
+            regionIndex = vorUpdated.point_region[i]
+            region = vorUpdated.regions[regionIndex]
+            vertices = vorUpdated.vertices[region + [region[0]], :]
+            areaUpdated = areaUpdated +  [sp.spatial.ConvexHull(vorUpdated.vertices[region, :]).volume]
+            #ax.plot(vertices[:, 0], vertices[:, 1], 'k-')
+
+        # print(len(area), len(areaUpdated))
+        if len(areaUpdated) < 19:
+            # print(point)
+            bezier_points = bezier_points[:-1]
+            slopeListX = slopeListX + [0]
+            slopeListY = slopeListY + [0]
+            failedVor = failedVor + 1
+            continue
+
+        for i in range(len(area)):
+            areaPercentage = areaPercentage + [abs(area[i] - areaUpdated[i]) / areaUpdated[-1]]
+
+        slopeAtPoint = 0
+
+        for i in range(len(areaPercentage)):
+            sumBernstein = 0
+            for y in range(gradeBezier - 1):
+                product = np.outer(bernstein(gradeBezier - 1, y, T[i]), P[y + 1] - P[y])
+                sumBernstein = sumBernstein + product
+            
+            sumBernstein = sumBernstein * gradeBezier
+            slopeAtPoint = slopeAtPoint + (areaPercentage[i] * sumBernstein)
+        
+        bezier_points = bezier_points[:-1]
+        # pl.pause(0.1)
+        # print(slopeAtPoint[0])
+        slopeListX = slopeListX + [slopeAtPoint[0][0]]
+        slopeListY = slopeListY + [slopeAtPoint[0][1]]
+
+    
+    else:
+        slopeListX = slopeListX + [0]
+        slopeListY = slopeListY + [0]
+        
+
+slopeArrX = np.array(slopeListX)
+slopeArrY = np.array(slopeListY)
+
+print(failedVor)
+u = slopeArrX.reshape(100, 100)
+v = slopeArrY.reshape(100, 100)
+
+# xv, yv = np.meshgrid(slopeListX, slopeListY)
+for simplex in hull.simplices:
+    ax.plot(bezier_points[simplex, 0], bezier_points[simplex, 1], 'b-', label='Convex Hull' if simplex[0] == hull.simplices[0][0] else "")
+        
+pl.streamplot(X, Y, u, v, color='black', density=4)
+pl.scatter(bezier_points[:, 0], bezier_points[:, 1], color='blue', s=50, label='Bézier Points')
+# pl.scatter(inside_hull[:, 0], inside_hull[:, 1], color='blue', alpha=0.3, s=1, label='Points Inside Hull')
+np.savetxt("gridpoint.txt", grid_points, fmt="%.6f", delimiter=",")
+
+pl.show()
+
